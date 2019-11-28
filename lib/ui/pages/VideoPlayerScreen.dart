8bd31e2eb5b06bc8d6959482002972444c36afae
sc4v3ng3r <scavenger.gnu@gmail.com>
2019-11-28 16:43:17 -0300

    handling orientation changes in video player page.

--- lib/ui/pages/VideoPlayerScreen.dart
+++ lib/ui/pages/VideoPlayerScreen.dart
@@ -28,6 +28,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   void initState() {
+    SystemChrome.setPreferredOrientations( DeviceOrientation.values );
+
     super.initState();
     appStore = Provider.of<ApplicationStore>(context, listen: false);
     videoPlayerStore = VideoPlayerStore(appStore);
@@ -39,17 +41,17 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
     return WillPopScope(
       onWillPop: () async {
+        print('OnWillPop');
         if (videoPlayerStore.episodeLoadingStatus == EpisodeLoading.LOADING) {
           videoPlayerStore.cancelEpisodeLoading();
         }
 
-        if (videoPlayerStore.episodeLoadingStatus == EpisodeLoading.DONE){
-          if (_chewieController.isFullScreen){
-            _chewieController.exitFullScreen();
-            return false;
-          }
+        else if (_chewieController.isFullScreen) {
+          _chewieController.exitFullScreen();
+          return true;
         }
 
+        await SystemChrome.setPreferredOrientations( [DeviceOrientation.portraitUp, ] );
         return true;
       },
 
@@ -58,7 +60,9 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
         body: Observer(
           builder: (context){
             if(videoPlayerStore.episodeLoadingStatus == EpisodeLoading.LOADING)
-              return Center(child: CircularProgressIndicator(),);
+              return Center(
+                child: CircularProgressIndicator(),
+              );
 
             if (videoPlayerStore.episodeLoadingStatus == EpisodeLoading.ERROR)
               return Column(
@@ -83,19 +87,17 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                 ],
               );
 
-            if (videoPlayerStore.episodeLoadingStatus == EpisodeLoading.CANCELED) {
-              print('episode loading cancelled');
+            if (videoPlayerStore.episodeLoadingStatus == EpisodeLoading.CANCELED)
               return Container();
-            }
 
             if (videoPlayerStore.episodeLoadingStatus == EpisodeLoading.DONE){
               _playerController ??= _createPlayerController(
                   videoPlayerStore.currentEpisode.streamingUrl, );
               _chewieController ??= _createChewieController();
+
             }
 
-            return Chewie(controller: _chewieController,);
-
+            return Chewie( controller: _chewieController, );
           },
         ),
       ),
@@ -104,35 +106,29 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   void dispose() {
-    SystemChrome.setPreferredOrientations(
-    [DeviceOrientation.portraitUp,]);
     _clearControllers();
     super.dispose();
   }
 
-  void playNext(){
-
-  }
+  void playNext(){}
 
   void playPrevious(){}
 
   VideoPlayerController _createPlayerController(String streamUrl) =>
       VideoPlayerController.network(streamUrl,
-
           httpHeaders: {'Referer': videoPlayerStore.currentEpisode.referer} );
-
 
   ChewieController _createChewieController() =>
       ChewieController(
         videoPlayerController: _playerController,
         aspectRatio: _DEFAULT_ASPECT_RATIO,
-        autoPlay: true,
         allowFullScreen: true,
+        fullScreenByDefault: true,
+        autoPlay: true,
         looping: false,
         showControls: true,
         allowedScreenSleep: false,
         allowMuting: true,
-        fullScreenByDefault: false,
         showControlsOnInitialize: true,
       );
 
@@ -142,4 +138,5 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     _chewieController = null;
     _playerController = null;
   }
+
 }

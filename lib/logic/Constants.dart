f2a554baea79cdacbbbc8b63b688dfc197b538af
sc4v3ng3r <scavenger.gnu@gmail.com>
2020-02-24 19:36:40 -0300

    adding video initialization timeout min and default const values.

--- lib/logic/Constants.dart
+++ lib/logic/Constants.dart
@@ -4,5 +4,10 @@ final IMAGE_BACKGROUND_COLOR =  Colors.grey[200];
 final ERROR_COLOR = Colors.red;
 const HERO_TAG_UPDATE = 'UPDATE';
 const HERO_TAG_RELEASE = 'RELEASE';
-const HER_TAG_CAROUSEL = 'CAROUSEL';
+const HERO_TAG_CAROUSEL = 'CAROUSEL';
 const HERO_TAG_MY_LIST = 'MYLISTTAG';
+
+/// The default video initialization timeout The.
+const VIDEO_INIT_TIMEOUT = 25;
+const VIDEO_INIT_TIMEOUT_MIN = 20;
+
